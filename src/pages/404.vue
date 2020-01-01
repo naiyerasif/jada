6de52c29f34687324086f10b663b3d8ef47ae175
Microflash <naiyer.app@gmail.com>
2020-01-01 23:20:16 +0530

    feat(ux): add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout>
+    <Hero>
+      <h1>Page not found</h1>
+    </Hero>
+    <div class="container">
+      <Panel>
+        <g-image src="../../static/404.svg" />
+      </Panel>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Hero from '~/components/Hero'
+import Panel from '~/components/Panel'
+
+export default {
+  metaInfo: {
+    title: 'Page not found'
+  },
+  components: {
+    Hero,
+    Panel
+  }
+}
+</script>
