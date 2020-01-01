64fa9aad9510546b8c460ef2cc017bbb5fc0e622
Microflash <naiyer.app@gmail.com>
2020-01-01 12:55:51 +0530

    feat(component): add hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container hero">
+    <h1>
+      <slot></slot>
+    </h1>
+    <div v-if="subtitleExists">
+      <slot name="subtitle"></slot>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    subtitleExists() {
+      return !!this.$slots.subtitle
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  @include spacing($gap-xs);
+  width: $width-full;
+
+  a {
+    &:focus,
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  h1 {
+    font-size: $text-xl;
+
+    @include tab-landscape {
+      font-size: $text-3xl;
+    }
+  }
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,21 @@
 <template>
   <Layout>
-    <g-image alt="logo" src="~/favicon.png" width="100" />
+    <Hero>
+      &mdash; A practical<br/>
+      blog starter for <a href="https://gridsome.org/" target="_blank" rel="noopener noreferrer">Gridsome</a>
+    </Hero>
   </Layout>
 </template>
 
 <script>
+import Hero from '~/components/Hero'
+
 export default {
   metaInfo: {
-    title: "Hello, world!"
+    title: 'Home'
+  },
+  components: {
+    Hero
   }
 }
 </script>
