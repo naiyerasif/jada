1120bac95cf31a87983621683b4dce7ed24e72bc
Microflash <naiyer.app@gmail.com>
2020-01-01 13:02:04 +0530

    chore(prep): add site metadata in head

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import '~/assets/styles/main.scss'
 
 import DefaultLayout from '~/layouts/Default.vue'
+import siteConfig from '../config/site.json'
 
 export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout)
+
+  head.meta.push({
+    name: 'author',
+    content: siteConfig.maintainer
+  })
 }
