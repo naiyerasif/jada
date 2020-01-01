80fbf815cbe8abe4c549194f153ccb8de29e447e
Microflash <naiyer.app@gmail.com>
2020-01-01 14:56:12 +0530

    feat(component): add panel component

---
+++ src/components/Panel.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="panel">
+    <main class="article decorated-links">
+      <slot></slot>
+    </main>
+    <section class="note decorated-links" v-if="noteExists">
+      <slot name="note"></slot>
+    </section>
+    <section class="card" v-if="cardExists">
+      <slot name="card"></slot>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    noteExists() {
+      return !!this.$slots.note
+    },
+    cardExists() {
+      return !!this.$slots.card
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.panel {
+  display: block;
+  overflow: hidden;
+  border-top: 1px solid $border-panel;
+  border-bottom: 1px solid $border-panel;
+
+  @include tab-portrait {
+    border-right: 1px solid $border-panel;
+    border-left: 1px solid $border-panel;
+    border-radius: $radius-base;
+  }
+}
+</style>
