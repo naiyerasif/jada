b300aae5f11bf8b92d85b060de7c9e90532cc95f
Microflash <naiyer.app@gmail.com>
2020-01-04 02:10:35 +0530

    chore(prep): prepare for search integration

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const fs = require('fs')
 const { GraphQLString } = require('gridsome/graphql')
 const appConfig = require('./app.config')
 const summarize = require('./marked.config').summarize
@@ -6,7 +8,7 @@ const editConfigs = appConfig.editConfig.paths
 const { basePath, constructEditUrl } = editConfigs.filter(p => p.collection === 'Post')[0]
 
 module.exports = function (api) {
-  api.loadSource(({ addSchemaResolvers, addCollection, getCollection }) => {
+  api.loadSource(({ addSchemaResolvers, getCollection }) => {
     const posts = getCollection('Post')
     posts.data().forEach(node => {
       if (!node.updated) {
@@ -30,5 +32,32 @@ module.exports = function (api) {
         }
       }
     })
+  })
+
+  api.beforeBuild(({ store }) => {
+    const { collection } = store.getCollection('Post')
+
+    const posts = collection.data.map(post => {
+      return {
+        title: post.title,
+        path: post.path,
+        blurb: post.blurb ? post.blurb : summarize(post.content)
+      }
+    })
+
+    const output = {
+      dir: `./${appConfig.searchConfig.file.dir}`,
+      name: appConfig.searchConfig.file.name
+    }
+
+    const outputPath = path.resolve(process.cwd(), output.dir)
+    const outputPathExists = fs.existsSync(outputPath)
+    const fileName = output.name.endsWith('.json') ? output.name : `${output.name}.json`
+
+    if (!outputPathExists) {
+      fs.mkdirSync(outputPath)
+    }
+
+    fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
   })
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import '~/assets/styles/main.scss'
 
+import VueFuse from 'vue-fuse'
 import DefaultLayout from '~/layouts/Default.vue'
 import siteConfig from '../data/site.json'
 
@@ -7,6 +8,8 @@ export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout)
 
   Vue.filter('clip', (value) => value.replace(/^(.{250}[^\s]*).*/, '$1'))
+
+  Vue.use(VueFuse)
 
   head.meta.push({
     name: 'author',
