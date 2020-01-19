012ac1443d89f75d13c12b84e310d9f558e66c58
Microflash <naiyer.app@gmail.com>
2020-01-19 16:40:23 +0530

    chore: move assets under dedicated directory

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -4,10 +4,10 @@
       <h1>Page not found</h1>
       <p>Go back to <g-link to="/">Home</g-link>.</p>
     </div>
-    <div class="container">
+    <div class="container wrapper">
       <div class="post">
         <g-link to="/" class="panel">
-          <g-image src="../../static/404.svg" />
+          <g-image src="../../static/assets/images/404.svg" />
         </g-link>
       </div>
     </div>
