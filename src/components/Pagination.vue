09d1ba5820292bd9e71909bee19dd6e302d69d8a
Microflash <naiyer.app@gmail.com>
2020-01-19 18:07:54 +0530

    feat(ux): remove unused styles and streamline spacing

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="container pagination">
-    <g-link class="is-prev" :to="previousPage(input.currentPage)" v-if="input.currentPage > 1"><svg viewBox="0 0 24 24" class="icon icon-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg> Prev</g-link>
+    <g-link class="is-prev" :to="previousPage(input.currentPage)" v-if="input.currentPage > 1"><svg viewBox="0 0 24 24" class="icon-sm icon-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg> Prev</g-link>
     <div>page {{ input.currentPage }} of {{ input.totalPages }}</div>
-    <g-link class="is-next" :to="nextPage(input.currentPage)" v-if="input.currentPage < input.totalPages">Next <svg viewBox="0 0 24 24" class="icon icon-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg></g-link>
+    <g-link class="is-next" :to="nextPage(input.currentPage)" v-if="input.currentPage < input.totalPages">Next <svg viewBox="0 0 24 24" class="icon-sm icon-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg></g-link>
   </section>
 </template>
 
