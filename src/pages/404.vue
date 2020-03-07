f3eedb774a91f57b37f5de7df6286c8f41cf7588
Microflash <naiyer.app@gmail.com>
2020-03-07 20:45:27 +0530

    feat: more 404 messages

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,8 +2,8 @@
   <Layout>
     <div class="container hero">
       <section class="canvas">
-        <h1 class="title" id="top">
-          Page not found
+        <h1 class="title">
+          <span class="is-warning">404</span> {{ getMsg() }}
         </h1>
         <div class="subtitle">
           <IconChevronLeft class="icon" /> Go back to <g-link to="/">Home</g-link>.
@@ -24,6 +24,13 @@
 import IconChevronLeft from '~/assets/images/icon-chevron-left.svg'
 import NotFound from '~/assets/images/404.svg'
 
+const msgs = [
+  `Page not found`,
+  `There's nothing here`,
+  `Looks like something went wrong`,
+  `This link seems broken `
+]
+
 export default {
   metaInfo: {
     title: 'Page not found'
@@ -31,6 +38,11 @@ export default {
   components: {
     IconChevronLeft,
     NotFound
+  },
+  methods: {
+    getMsg () {
+      return msgs[Math.floor(Math.random() * msgs.length)]
+    }
   }
 }
 </script>
